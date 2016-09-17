ddb711ecd923aef8e5e54bf05a00675b1f5e33df
XionKannagi <0906chachamaru@gmail.com>
2016-09-18 00:56:07 +0900

    SceneAnalyzer implements

--- app/src/main/java/zack/inc/jp/studytest2/SceneAnalyzer.java
+++ app/src/main/java/zack/inc/jp/studytest2/SceneAnalyzer.java
@@ -15,35 +15,65 @@ public class SceneAnalyzer {
     public int judgeStatus() {
         if (Az < 0.4f && speed <= 1.4) {
             return 1;//停車中が状態orブレーキ終了状態１->return 1
-        } else if (speed > 1.4) {
+        } else if (speed > 1.4 && Az < 0.4f) {
             return 2;//走行中が状態２->return 2
         } else {
             return 3;//ブレーキ中が状態３->return 3
         }
-
     }
 
-    public void mainfunc(float aX, float aY, float aZ, double v) {
+    // 2 -> 3 -> 1
+
+    private boolean inState1, inState3;
+    private float max;
+
+    public boolean mainfunc(float aX, float aY, float aZ, double v) {
         Ax = aX;
         Ay = aY;
         Az = aZ;
         speed = v;
 
-
         result = judgeStatus();
 
-        if (result == 1){
+        if (result == 1) {
 
-        } else  if(result == 2) {
+            inState1 = true;
+            inState3 = false;
+
+            if (inState3 == true) {
+                //end_time = getTime();
+                return true; // 状態3 直後の 状態1
+            }
+
+        } else if (result == 2) {
+
+            inState1 = false;
+            inState3 = false;
 
         } else {
-            //TODO ブレーキ中の挙動->加速度のピークとその時の時間を計測
-        }
+            //TODO ブレーキ中の挙動->加速度のピークとその時の時間を
+            if (inState3 == false) {
+                //start_time = getTime(); // 初めての状態3
+            }
 
+            inState1 = false;
+            inState3 = true;
+
+            if (max < Az) {
+                max = Az;
+            }
+        }
+        return false;
     }
 
 
-
-
+    private void get(){
+        //開始速度
+        //開始位置
+        //終了速度
+        //終了位置
+        //ピーク値
+        //ピーク時刻
+    }
 
 }
